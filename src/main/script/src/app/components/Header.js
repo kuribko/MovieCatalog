36813a87a20a755c8fe03786eff22bbef71d2fba
kuribko <konstantin.kuribko@gmail.com>
2017-03-18 03:33:12 +0200

    js: reflexive ui improvements

--- src/main/script/src/app/components/Header.js
+++ src/main/script/src/app/components/Header.js
@@ -1,20 +1,19 @@
 import React from "react";
 import Search from "../containers/Search";
 import Synchronizer from "../containers/Synchronizer";
-import Sorter from "../containers/Sorter";
 
 export const Header = (props)=> {
     return (
         <header>
                 <nav className="container navbar-inverse navbar-fixed-top">
                     <div className="container-fluid">
-                        <div className="navbar-header col-lg-3 col-md-3 col-sm-3 col-xs-0">
+                        <div className="navbar-header col-lg-3 col-md-3 col-sm-0 col-xs-0">
                             <a className="navbar-brand" href="/">Movie Catalog</a>&nbsp;
                         </div>
 
                         <Search/>
 
-                        <div className="nav navbar-right col-md-3 col-md-3 col-sm-3 col-xs-4 ">
+                        <div className="nav navbar-right col-md-3 col-md-3 col-sm-12 col-xs-12 ">
                             <Synchronizer/>
                         </div>
                     </div>
--- src/main/script/src/app/containers/Search.js
+++ src/main/script/src/app/containers/Search.js
@@ -12,7 +12,7 @@ class Search extends React.Component {
 
     render() {
         return (
-            <div id="search" className="input-group col-lg-6 col-md-6 col-sm-6 col-xs-8">
+            <div id="search" className="input-group col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
                 <div id="count" className="input-group-addon">
                     <span className="small">
